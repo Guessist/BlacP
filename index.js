f2ce2257b201a63345b17d8b155312ecc5861562
guessist25 <homp3@naver.com>
2022-09-03 09:48:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// var moment = require('moment');
 moment().format();
 
 function changeDeltaText(delta) {
@@ -25,33 +24,98 @@ function changeEndDateText(endDate) {
 function calculate() {
     let joinDateString = $('#join_date').val();
     let taljuDateString = $('#talju_date').val();
-    let rejoinDateString = $('#rejoin_date').val();  
-    
+    let rejoinDateString = $('#rejoin_date').val();
+
     let joinDate = moment(joinDateString, 'YYMMDD');
     let taljuDate = moment(taljuDateString, 'YYMMDD');
     let rejoinDate = moment(rejoinDateString, 'YYMMDD');
 
-    alert(moment(joinDate))
-    alert(moment(taljuDate))
-    alert(moment(rejoinDate))
-    
+
     if (!joinDate.isValid()) {
         alert('날짜를 올바르게 입력하여 주세요 (예. 170101)');
         return;
     }
-    
+    if (!taljuDate.isValid() || !rejoinDate.isValid() ) {
+
+        let cnt = 1;
+        let appCnt = 1;
+
+        let joinDate_;
+        joinDate_= moment(joinDate).format();
+        while (appCnt < 180) {
+            let cntDate;
+            cntDate = moment(joinDate_).add(cnt, "days").format('YYMMDD');
+//            alert(cntDate);
+            if(moment(cntDate).isBefore('220124')) appCnt++;
+            if(moment(cntDate).isAfter('220401') && moment(cntDate).isBefore('220704')) appCnt++;
+            if(moment(cntDate).isAfter('220729')) appCnt++;
+            cnt++;
+
+        }
+        alert('결과')
+        //alert(String(appCnt))
+        //alert(String(cnt))
+        let certificateDate;
+        let joinDate2 = moment(joinDate).format()
+        certificateDate = moment(joinDate2).add(cnt, "days").format('YYMMDD');
+        alert('신청가능일은');
+        alert(certificateDate);
+        alert('입니다');
+
+    }
+    else { // TODO 탈주일 포함된경우
+        alert(' TODO 탈주일 포함된경우')
+        let cnt1 = 1;
+        let appCnt1 = 1;
+
+        let joinDate_;
+        let taljuDate_;
+        let rejoindate_;
+        joinDate_= moment(joinDate).format();
+        taljuDate_= moment(taljuDate).format('YYMMDD');
+        rejoindate_= moment(rejoinDate).format('YYMMDD');
+        while (appCnt1 < 180) {
+            let cntDate;
+            cntDate = moment(joinDate_).add(cnt1, "days").format('YYMMDD');
+//            alert(cntDate);
+            if(moment(cntDate).isBefore('220124')) {
+                if(moment(cntDate).isAfter(moment(taljuDate_)) && moment(cntDate).isBefore(moment(rejoindate_))) {}
+                else appCnt1++;
+            }
+            if(moment(cntDate).isAfter('220401') && moment(cntDate).isBefore('220704')) {
+                if(moment(cntDate).isAfter(moment(taljuDate_)) && moment(cntDate).isBefore(moment(rejoindate_))) {}
+                else appCnt1++;
+            }
+            if(moment(cntDate).isAfter('220729')) {
+                if(moment(cntDate).isAfter(moment(taljuDate_)) && moment(cntDate).isBefore(moment(rejoindate_))) {}
+                else appCnt1++;
+            }
+            cnt1++;
+        }
+        alert('결과')
+        alert(String(cnt1))
+        let certificateDate;
+        let joinDate2 = moment(joinDate).format()
+        certificateDate = moment(joinDate2).add(cnt1, "days").format('YYMMDD');
+        alert(certificateDate);
+
+
+    }
 
     $('.results').hide().fadeIn(1200).show();
 
-    //let referenceDate; // Date when policy first applied
-    //let prevEndDate;
-
     let serviceType = $('.select_service_type').val();
-    
+
     let cntDate;
     let cnt = 1;
     let appCnt = 1;
-    
+//
+//    alert('12');
+//    alert(joinDate);
+//    let joinDate__;
+//    joinDate__= moment(joinDate).format('YYMMDD');
+//    if(moment(joinDate__).isSameOrAfter('220124'))    alert('ab');
+
    // if(jointDate.isSameOrAfter("2022-01-24") && jointDate.isSameOrBefore("2022-04-01")) {
    //     appCnt = 0;
     //}
@@ -67,19 +131,17 @@ function calculate() {
         //alert('테스트');
        // alert('test')
        // alert(String(appCnt))
-
-        
-
-    } else if (serviceType === 'navy') {
+    }
+    else if (serviceType === 'navy') {
         //prevEndDate.month(prevEndDate.month() + 23);
        // referenceDate = moment("161103", 'YYMMDD');
-        
-
-    } else if (serviceType === 'social_service') {
+    }
+    else if (serviceType === 'social_service') {
         //prevEndDate.year(prevEndDate.year() + 2);
        // referenceDate = moment("161003", 'YYMMDD');
 
-    } else {
+    }
+    else {
         //prevEndDate.month(prevEndDate.month() + 21);
        // referenceDate = moment("170103", 'YYMMDD');
     }
